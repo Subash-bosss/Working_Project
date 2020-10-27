a3363f737064424265bb022375113204457e0562
subash bose <netajisubash1997@gmail.com>
2020-10-27 12:55:59 +0530

    react added

---
+++ src/main/resources/static/precache-manifest.95338754509ea0c3f515cd2681095238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c21513c5657d8c833b19edc282ce5ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9f5f058846744a4e21",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "58809a9f1c8e21dab7d6",
+    "url": "/static/css/main.7c4f406b.chunk.css"
+  },
+  {
+    "revision": "9e9f5f058846744a4e21",
+    "url": "/static/js/2.e93f76f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e93f76f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58809a9f1c8e21dab7d6",
+    "url": "/static/js/main.d71fffe5.chunk.js"
+  },
+  {
+    "revision": "52c76779b094319d3896",
+    "url": "/static/js/runtime-main.9bc5bc49.js"
+  }
+]);
